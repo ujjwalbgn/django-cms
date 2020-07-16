b7f219c2572c3fe82cdc064840dee7991e694300
Ujjwal Bajagain <ubajagain@gmail.com>
2020-07-16 18:42:58 -0500

    tempaltes added

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+# Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('Product Page')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('Customer Page')
+    return render(request, 'accounts/customer.html')
