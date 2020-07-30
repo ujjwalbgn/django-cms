cbdbcb5e44e5d627351d798d5e2d8921bfb43486
Ujjwal Bajagain <ubajagain@gmail.com>
2020-07-29 22:46:12 -0500

    User customer relation added

--- accounts/views.py
+++ accounts/views.py
@@ -27,6 +27,10 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name = 'customer')   #adds new registered to customer group
             user.groups.add(group)
+            Customer.objects.create(
+                user = user,
+            )
+
             messages.success(request,'Account was created for ' + username)
 
             return redirect('login')
@@ -72,14 +76,20 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
-
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders,'total_orders': total_orders, 'delivered' : delivered, 'pending' : pending}
     return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
-
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products' : products})
