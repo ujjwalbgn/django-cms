01b78c5f4ffd9f96d60af9d9b62d13fbdcbefcf7
Ujjwal Bajagain <ubajagain@gmail.com>
2020-07-30 22:56:05 -0500

    user profile pic upload function added

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-def unauthenticated_user(view_fun):
+def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
-
         if request.user.is_authenticated:
             return redirect('home')
-
         else:
-            return view_fun(request, *args, **kwargs)
+            return view_func(request, *args, **kwargs)
 
     return wrapper_func
 
@@ -18,24 +16,25 @@ def allowed_users(allowed_roles=[]):
 
             group = None
             if request.user.groups.exists():
-                group = request.user .groups.all()[0].name
+                group = request.user.groups.all()[0].name
 
-                if group in allowed_roles:
-                    return view_func(request, *args, **kwargs)
-                else:
-                    return HttpResponse('You are not authorized to view this page')
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
 
-def admin_only(view_funcs):
+def admin_only(view_func):
     def wrapper_function(request, *args, **kwargs):
         group = None
         if request.user.groups.exists():
             group = request.user.groups.all()[0].name
 
-            if group == 'customer':
-                return redirect('user-page')
+        if group == 'customer':
+            return redirect('user-page')
 
-            if group == 'admin':
-                return view_funcs(request, *args, **kwargs)
-    return  wrapper_function
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_function
