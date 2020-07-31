85bb8c72481b1819edcdf135a008ff30859533be
Ujjwal Bajagain <ubajagain@gmail.com>
2020-07-31 02:18:34 -0500

    user password reset email added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
@@ -16,5 +18,12 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name= "create_order" ),
     path('update_order/<str:pk>', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>', views.deleteOrder, name="delete_order"),
+
+
+    #password reset
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),name ="password_reset"),
+    path('reset_password_set/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name= "password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_complete"),
 
 ]
